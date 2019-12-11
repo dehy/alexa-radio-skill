9eadc9504267a4a66515a8f1e56e26818c28f4e3
Arnaud de Mouhy <arnaud@admds.net>
2019-12-11 14:41:39 +0100

    Updated guzzle-http and enhanced Twig usage

--- config/bundles.php
+++ config/bundles.php
@@ -4,6 +4,6 @@ return [
     MaxBeckers\AmazonAlexaBundle\MaxBeckersAmazonAlexaBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
